69c0e69b37e94ad32bff428d0f89e453c3843299
MikkelyEV <74329707+MikkelyEV@users.noreply.github.com>
2021-01-31 01:04:04 -0700

    muxhoscambios

--- DojaCafe/DojaCafe/Cliente.cs
+++ DojaCafe/DojaCafe/Cliente.cs
@@ -16,5 +16,21 @@ namespace DojaCafe
         {
             InitializeComponent();
         }
+
+        private void guna2TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2PictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_bitacora_Click(object sender, EventArgs e)
+        {
+            Bitacora_Clientes bitacora_cli = new Bitacora_Clientes();
+            bitacora_cli.Show();
+        }
     }
 }
