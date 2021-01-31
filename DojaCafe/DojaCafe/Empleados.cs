5cf84ddb70a7f22204da8aebfcf12ea1a931411f
Kev <ikevinhuerta@gmail.com>
2021-01-30 23:43:34 -0700

    diseno empleado

--- DojaCafe/DojaCafe/Empleados.cs
+++ DojaCafe/DojaCafe/Empleados.cs
@@ -16,5 +16,55 @@ namespace DojaCafe
         {
             InitializeComponent();
         }
+
+        private void nombre_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Precio_lbl_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtb_id_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb_precio_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtb_nombre_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txb_desc_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Empleados_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2HtmlLabel4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
