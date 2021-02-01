9fb60dbd53b2e6732ea2dbd9d0c0d751c3220302
MikkelyEV <74329707+MikkelyEV@users.noreply.github.com>
2021-02-01 07:23:30 -0700

    Revert "Ramacodigo"

--- DojaCafe/DojaCafe/Bitacora_ventas.Designer.cs
+++ DojaCafe/DojaCafe/Bitacora_ventas.Designer.cs
@@ -29,12 +29,6 @@ namespace DojaCafe
         /// </summary>
         private void InitializeComponent()
         {
-<<<<<<< HEAD
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Bitacora_ventas));
-=======
             this.components = new System.ComponentModel.Container();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
@@ -47,7 +41,6 @@ namespace DojaCafe
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             this.dgv_ventas = new Guna.UI2.WinForms.Guna2DataGridView();
             this.vENTABindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.dOJACAFEDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
@@ -73,11 +66,6 @@ namespace DojaCafe
             // 
             // dgv_ventas
             // 
-<<<<<<< HEAD
-            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(237)))), ((int)(((byte)(183)))));
-            this.dgv_ventas.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
-            this.dgv_ventas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-=======
             this.dgv_ventas.AllowUserToAddRows = false;
             this.dgv_ventas.AllowUserToDeleteRows = false;
             this.dgv_ventas.AllowUserToResizeColumns = false;
@@ -85,45 +73,18 @@ namespace DojaCafe
             dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(237)))), ((int)(((byte)(183)))));
             this.dgv_ventas.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
             this.dgv_ventas.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.ColumnHeader;
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             this.dgv_ventas.BackgroundColor = System.Drawing.Color.White;
             this.dgv_ventas.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dgv_ventas.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
             this.dgv_ventas.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
-<<<<<<< HEAD
-            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
-            dataGridViewCellStyle2.Font = new System.Drawing.Font("Montserrat", 11.25F);
-=======
             dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
             dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(196)))), ((int)(((byte)(15)))));
             dataGridViewCellStyle2.Font = new System.Drawing.Font("Montserrat SemiBold", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
             dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
             this.dgv_ventas.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
-<<<<<<< HEAD
-            this.dgv_ventas.ColumnHeadersHeight = 4;
-            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
-            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(251)))), ((int)(((byte)(243)))), ((int)(((byte)(207)))));
-            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
-            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(215)))), ((int)(((byte)(95)))));
-            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
-            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dgv_ventas.DefaultCellStyle = dataGridViewCellStyle3;
-            this.dgv_ventas.EnableHeadersVisualStyles = false;
-            this.dgv_ventas.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(233)))), ((int)(((byte)(170)))));
-            this.dgv_ventas.Location = new System.Drawing.Point(16, 4);
-            this.dgv_ventas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.dgv_ventas.Name = "dgv_ventas";
-            this.dgv_ventas.RowHeadersVisible = false;
-            this.dgv_ventas.RowHeadersWidth = 51;
-            this.dgv_ventas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgv_ventas.Size = new System.Drawing.Size(752, 546);
-=======
             this.dgv_ventas.ColumnHeadersHeight = 30;
             this.dgv_ventas.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.Column1,
@@ -147,7 +108,6 @@ namespace DojaCafe
             this.dgv_ventas.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders;
             this.dgv_ventas.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgv_ventas.Size = new System.Drawing.Size(647, 444);
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             this.dgv_ventas.TabIndex = 0;
             this.dgv_ventas.Theme = Guna.UI2.WinForms.Enums.DataGridViewPresetThemes.SunFlower;
             this.dgv_ventas.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(237)))), ((int)(((byte)(183)))));
@@ -194,7 +154,7 @@ namespace DojaCafe
             this.btn_volver.AutoRoundedCorners = true;
             this.btn_volver.BackColor = System.Drawing.Color.Transparent;
             this.btn_volver.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(151)))), ((int)(((byte)(145)))));
-            this.btn_volver.BorderRadius = 22;
+            this.btn_volver.BorderRadius = 17;
             this.btn_volver.BorderThickness = 1;
             this.btn_volver.CheckedState.Parent = this.btn_volver;
             this.btn_volver.CustomImages.Parent = this.btn_volver;
@@ -204,26 +164,18 @@ namespace DojaCafe
             this.btn_volver.HoverState.Parent = this.btn_volver;
             this.btn_volver.Image = ((System.Drawing.Image)(resources.GetObject("btn_volver.Image")));
             this.btn_volver.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-<<<<<<< HEAD
-            this.btn_volver.Location = new System.Drawing.Point(875, 367);
-            this.btn_volver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-=======
             this.btn_volver.Location = new System.Drawing.Point(656, 388);
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             this.btn_volver.Name = "btn_volver";
             this.btn_volver.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(50)))), ((int)(((byte)(39)))));
             this.btn_volver.ShadowDecoration.BorderRadius = 22;
             this.btn_volver.ShadowDecoration.Color = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(44)))), ((int)(((byte)(34)))));
             this.btn_volver.ShadowDecoration.Enabled = true;
             this.btn_volver.ShadowDecoration.Parent = this.btn_volver;
-            this.btn_volver.Size = new System.Drawing.Size(151, 46);
+            this.btn_volver.Size = new System.Drawing.Size(113, 37);
             this.btn_volver.TabIndex = 62;
             this.btn_volver.Text = "Volver";
             this.btn_volver.TextOffset = new System.Drawing.Point(10, 0);
-<<<<<<< HEAD
-=======
             this.btn_volver.Click += new System.EventHandler(this.btn_volver_Click);
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             // 
             // btn_delete
             // 
@@ -231,7 +183,7 @@ namespace DojaCafe
             this.btn_delete.AutoRoundedCorners = true;
             this.btn_delete.BackColor = System.Drawing.Color.Transparent;
             this.btn_delete.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(151)))), ((int)(((byte)(145)))));
-            this.btn_delete.BorderRadius = 22;
+            this.btn_delete.BorderRadius = 17;
             this.btn_delete.BorderThickness = 1;
             this.btn_delete.CheckedState.Parent = this.btn_delete;
             this.btn_delete.CustomImages.Parent = this.btn_delete;
@@ -241,19 +193,14 @@ namespace DojaCafe
             this.btn_delete.HoverState.Parent = this.btn_delete;
             this.btn_delete.Image = ((System.Drawing.Image)(resources.GetObject("btn_delete.Image")));
             this.btn_delete.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
-<<<<<<< HEAD
-            this.btn_delete.Location = new System.Drawing.Point(875, 94);
-            this.btn_delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-=======
             this.btn_delete.Location = new System.Drawing.Point(656, 325);
->>>>>>> dad7967a04f3bae900b7a5a7236c35166afb9a3f
             this.btn_delete.Name = "btn_delete";
             this.btn_delete.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(50)))), ((int)(((byte)(39)))));
             this.btn_delete.ShadowDecoration.BorderRadius = 22;
             this.btn_delete.ShadowDecoration.Color = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(44)))), ((int)(((byte)(34)))));
             this.btn_delete.ShadowDecoration.Enabled = true;
             this.btn_delete.ShadowDecoration.Parent = this.btn_delete;
-            this.btn_delete.Size = new System.Drawing.Size(151, 46);
+            this.btn_delete.Size = new System.Drawing.Size(113, 37);
             this.btn_delete.TabIndex = 59;
             this.btn_delete.Text = "Eliminar";
             this.btn_delete.TextOffset = new System.Drawing.Point(10, 0);
@@ -357,16 +304,15 @@ namespace DojaCafe
             // 
             // Bitacora_ventas
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(122)))), ((int)(((byte)(81)))));
-            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.btn_volver);
             this.Controls.Add(this.btn_delete);
             this.Controls.Add(this.dgv_ventas);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Bitacora_ventas";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Bitacora Ventas";
